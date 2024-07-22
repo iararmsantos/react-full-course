38766fe30e95c5fe66f538ec2273a960590ff2ab
Iara Santos <iara.santos@thinkon.com>
2024-07-22 18:37:37 -0400

    Added gitignore

---
+++ 12_Classes/src/components/User.js
@@ -0,0 +1,16 @@
+import classes from "./User.module.css";
+import { Component } from "react";
+
+class User extends Component {
+  // constructor() {}
+  render() {
+    //this.props can be accessed because we extended Components
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
